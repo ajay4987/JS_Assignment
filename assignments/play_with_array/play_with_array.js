b0683f86c50dc6ad3db1179d77dcf91964ac903b
ajay_4987 <ajaysb.1610@gmail.com>
2022-03-06 13:25:54 +0530

    play with array submission

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,7 +7,13 @@ function getEven(arr) {
 
         Write you code below
     */
-
+   let res = []
+   for(let i=0;i<arr.length;i++){
+       if((arr[i]%2)==0){
+           res.push(arr[i])
+       }
+   }
+   return res;
 }
 
 function multiplyByN(arr, n) {
@@ -18,6 +24,11 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+   let res=[]
+   for(let i=0;i<arr.length;i++){
+       res.push(arr[i]*n)
+   }
+   return res;
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +39,8 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+    arr.splice(n,1);
+    return arr;
 }
 
 module.exports = {
